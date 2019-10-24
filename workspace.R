3e72c8f29acf708f1f2a89a0284f909bb83edc0a
Chris Russo <crusso@wesleyan.edu>
2019-10-24 12:58:56 -0400

    Updated workspace

--- workspace.R
+++ workspace.R
@@ -5,24 +5,59 @@ library(dplyr)
 
 load("pbp2019.rda")
 
-df <- pbp2019[1:100,] %>%
-  filter(as.numeric(launch_speed) > 0)
+# convert all null to NA
+pbp2019[pbp2019 == "null"] <- NA
 
-df$hc_x <- as.numeric(df$hc_x)
-df$hc_y <- as.numeric(df$hc_y)
+# order
+pbp2019 <- pbp2019 %>%
+  mutate(game_date = as.Date(game_date, "%m/%d/%Y")) %>%
+  arrange(game_date, home_team, at_bat_number, pitch_number)
 
-df %>% 
+# add indicator for stadium
+team_ids <- data.frame(team = unique(MLBStadiumsPathData$team)[-31],
+                       abbr = c("LAA","HOU","OAK","TOR","ATL",
+                                "MIL","STL","CHC","ARI","LAD",
+                                "SF","CLE","SEA","MIA","NYM",
+                                "WSH","BAL","SD","PHI","PIT",
+                                "TEX","TB","BOS","CIN","COL",
+                                "KC","DET","MIN","CWS","NYY"),
+                       stringsAsFactors = F)
+
+pbp2019 <- pbp2019 %>%
+  left_join(team_ids, by = c("home_team" = "abbr"))
+
+# convert variable types
+pbp2019 <- pbp2019 %>%
+  mutate(hc_x = as.numeric(hc_x),
+         hc_y = as.numeric(hc_y),
+         launch_speed = as.numeric(launch_speed))
+
+
+# select Pete Alonso batted balls home/away
+alonso_home <- pbp2019 %>%
+  filter(batter == 624413,
+         home_team == "NYM",
+         launch_speed > 0)
+
+alonso_away <- pbp2019 %>%
+  filter(batter == 624413,
+         home_team != "NYM",
+         launch_speed > 0)
+
+# plot
+alonso_home %>% 
   ggplot(aes(x=hc_x, y=hc_y)) + 
   geom_spraychart() 
 
-df %>% 
+alonso_home %>% 
   ggplot(aes(x=hc_x, y=hc_y)) + 
   geom_spraychart() + 
   theme_void() + 
   coord_fixed()
 
-df %>% mlbam_xy_transformation() %>%  
-  ggplot(aes(x=hc_x, y=hc_y)) + 
+
+alonso_home %>% mlbam_xy_transformation() %>%  
+  ggplot(aes(x=hc_x_, y=hc_y_, color = events)) + 
   geom_spraychart(stadium_ids = "mets",
                   stadium_transform_coords = TRUE, 
                   stadium_segments = "all") + 
@@ -30,11 +65,15 @@ df %>% mlbam_xy_transformation() %>%
   coord_fixed()
 
 
-df %>% mlbam_xy_transformation() %>%  
-  ggplot(aes(x=hc_x_, y=hc_y_, color = des)) + 
-  geom_spraychart(stadium_ids = "mets",
+alonso_away %>% mlbam_xy_transformation() %>%  
+  ggplot(aes(x=hc_x_, y=hc_y_, color = events)) + 
+  geom_spraychart(stadium_ids = unique(alonso_away$team),
                   stadium_transform_coords = TRUE, 
                   stadium_segments = "all") + 
   theme_void() + 
-  coord_fixed()
+  coord_fixed() +
+  facet_wrap(~team)
+
+
+
 
